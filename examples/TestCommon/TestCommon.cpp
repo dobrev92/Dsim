eeed4e38ccb25919bb1b1fcc62e3639cafb67b33
George Dobrev <georgi.dobrev1992@gmail.com>
2018-01-02 11:15:06 +0100

    TestCommon.h, TestCommon.cpp:
    Move the callbacks from DetTest. Add a
    function to update the camera.

--- examples/TestCommon/TestCommon.cpp
+++ examples/TestCommon/TestCommon.cpp
@@ -13,13 +13,26 @@ TestCommon::TestCommon()
 
 	InitWindow();
 	InitRender();
+
+	Root = new Node;
+	Camera = new Node;
+	Root->AddChild(Camera);
+
+	Camera->Translate(0.0f, 0.0f, 5.0f);
 }
 
 ds_err TestCommon::InitWindow()
 {
 	dbg_info("\n");
 	window = new GraphicsWindowGLFW;
-	window->Init(false, 4, 848, 480);
+	window->Init(true, 4, 1600, 900);
+
+	//Init the callbacks
+	window->SetMouseMoveCallback(TestCommon::mSMouseMoveCallback);
+	window->SetMouseButtonCallback(TestCommon::mSMouseButtonCallback);
+	window->SetWindowSizeCallback(TestCommon::mSWindowResizeCallback);
+	window->SetCalleeObj(this);
+
 	return DS_SUCCESS;
 }
 
@@ -40,11 +53,48 @@ TestCommon::~TestCommon()
 	if (window) {
 		delete window;
 	}
+
+	if (Root) {
+		delete Root;
+	}
+}
+
+void TestCommon::UpdateCamera()
+{
+	double xMov, yMov;
+
+	window->GetRelativeMouseMovement(&xMov, &yMov);
+	if (xMov) {
+		Vector3 axis(0.0f, 1.0f, 0.0f);
+		Camera->Rotate(axis, 0.001f * xMov);
+	}
+
+	if (yMov) {
+		Vector3 axis(1.0f, 0.0f, 0.0f);
+		Camera->Rotate(axis, 0.001f * yMov);
+	}
+
+	if(window->GetKey(DS_KEY_W) == DS_KEY_PRESS) {
+		Camera->Translate(0.0f, 0.0f, - 0.1f, TS_LOCAL);
+	}
+
+	if(window->GetKey(DS_KEY_S) == DS_KEY_PRESS) {
+		Camera->Translate(0.0f, 0.0f, 0.1f, TS_LOCAL);
+	}
+
+	if(window->GetKey(DS_KEY_A) == DS_KEY_PRESS) {
+		Camera->Translate(- 0.1f, 0.0f, 0.0f, TS_LOCAL);
+	}
+
+	if(window->GetKey(DS_KEY_D) == DS_KEY_PRESS) {
+		Camera->Translate(0.1f, 0.0f, 0.0f, TS_LOCAL);
+	}
 }
 
 ds_err TestCommon::Run()
 {
 	while (window->IsActive()) {
+		UpdateCamera();
 		MainLoop();
 		render->RenderFrame();
 	}
@@ -52,3 +102,40 @@ ds_err TestCommon::Run()
 	return DS_SUCCESS;
 }
 
+//MEMBER CALLBACKS
+void TestCommon::mMouseMoveCallback(double xpos, double ypos)
+{
+	//dbg_info("\n"
+}
+
+void TestCommon::mMouseButtonCallback(int button, int action, int mods)
+{
+	//dbg_info("\n");
+}
+
+void TestCommon::mWindowResizeCallback(int width, int height)
+{
+	//dbg_info("\n");
+	//Matrix4x4PerspectiveFov(&Proj, PI / 4, (scalar)width / (scalar)height, 1, 30);
+	render->Viewport(0, 0, width, height);
+}
+
+
+//STATIC CALLBACKS
+void TestCommon::mSMouseMoveCallback(void *p_obj, double xpos, double ypos)
+{
+	TestCommon *obj = (TestCommon *)p_obj;
+	obj->mMouseMoveCallback(xpos, ypos);
+}
+
+void TestCommon::mSMouseButtonCallback(void *p_obj, int button, int action, int mods)
+{
+	TestCommon *obj = (TestCommon *)p_obj;
+	obj->mMouseButtonCallback(button, action, mods);
+}
+
+void TestCommon::mSWindowResizeCallback(void *p_obj, int width, int height)
+{
+	TestCommon *obj = (TestCommon *)p_obj;
+	obj->mWindowResizeCallback(width, height);
+}
--- examples/TestCommon/TestCommon.h
+++ examples/TestCommon/TestCommon.h
@@ -2,7 +2,8 @@
 #define __TEST_COMMON_H__
 
 #include "Render/GL/GLRender.h"
-#include "GraphicsWindowGLFW.h"
+#include "GraphicsWindow.h"
+#include "Node.h"
 
 class TestCommon {
 	protected:
@@ -10,8 +11,19 @@ class TestCommon {
 		Dsim::GLRender *render;
 
 		virtual Dsim::ds_err InitResources()=0;
+		virtual void UpdateCamera();
 		Dsim::ds_err InitWindow();
 		Dsim::ds_err InitRender();
+		Dsim::Node *Root;
+		Dsim::Node *Camera;
+
+		void mMouseMoveCallback(double xpos, double ypos);
+		void mMouseButtonCallback(int button, int action, int mods);
+		void mWindowResizeCallback(int width, int height);
+
+		static void mSMouseMoveCallback(void *p_obj, double xpos, double ypos);
+		static void mSMouseButtonCallback(void *p_obj, int button, int action, int mods);
+		static void mSWindowResizeCallback(void *p_obj, int width, int height);
 	public:
 		TestCommon();
 		virtual void MainLoop()=0;
